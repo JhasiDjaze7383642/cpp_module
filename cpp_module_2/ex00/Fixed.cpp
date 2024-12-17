4091530522d04110c21021657f3e170bca21e124
Ny Hasina <rarakoto@c1r12s5.42antananarivo.mg>
2024-12-17 10:26:22 +0300

    cpp_module_2/ex00/ Need to be checked

---
+++ cpp_module_2/ex00/Fixed.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 09:20:15 by rarakoto          #+#    #+#             */
+/*   Updated: 2024/12/17 10:22:11 by rarakoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : value(0), fractionnal_value(8)
+{
+	std::cout << "Default constructor called.\n";
+};
+
+Fixed::Fixed(int value) : value(value * (1 << 8)), fractionnal_value(8)
+{
+	std::cout << "Default constructor called.\n";
+};
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Deconstructor called.\n";
+}
+
+Fixed::Fixed(const Fixed &other) : value(other.value), fractionnal_value(8)
+{
+	std::cout << "Copy constructor called.\n";
+};
+
+Fixed&	Fixed::operator=(const Fixed &other)
+{
+	if (this != &other)
+		this->value = other.value;
+	std::cout << "Copy assignement operator called.\n";
+	return (*this);
+}
+
+int		Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called.\n";
+	return (this->value >> this->fractionnal_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->value = raw;
+}
