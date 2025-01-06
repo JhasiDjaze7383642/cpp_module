39b98195e44f4e133ce63c0d42598091467e43fc
Ny Hasina <rarakoto@c1r10s16.42antananarivo.mg>
2025-01-06 09:56:16 +0300

    Changing the message prompted to the constructor and destructor in the previous exercice so it match what is required in the subject

--- cpp_module_3/ex01/ScavTrap.cpp
+++ cpp_module_3/ex01/ScavTrap.cpp
@@ -6,15 +6,16 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 09:26:43 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/06 09:30:31 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/06 09:52:32 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap(void) : ClapTrap("")
+ScavTrap::ScavTrap(void) : ClapTrap("Default Name")
 {
-	std::cout << "Default constructor of ScavTrap called.\n";
+	std::cout << "ScavTrap with no name just popped of nowhere\n";
+	setName("Default Name");
 	setHitPoints(100);
 	setEnergyPoints(50);
 	setAttackDamage(20);
@@ -22,7 +23,8 @@ ScavTrap::ScavTrap(void) : ClapTrap("")
 
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 {
-	std::cout << "Parameterized constructor of ScavTrap called.\n";
+	std::cout << "ScavTrap " << name << " was instantiated\n";
+	setName(name);
 	setHitPoints(100);
 	setEnergyPoints(50);
 	setAttackDamage(20);
@@ -52,14 +54,14 @@ ScavTrap	&ScavTrap::operator=(const ScavTrap &other)
 
 ScavTrap::~ScavTrap(void)
 {
-	std::cout << "ScavTrap " << getName() << " have been destroyed\n";
+	std::cout << "ScavTrap " << getName() << " was erased from memory\n";
 };
 
 void		ScavTrap::attack(const std::string &target)
 {
 	if (getEnergyPoints() > 0)
 	{
-		std::cout << "ScavTrap " << getName() << " attacks " << target << ", causing " << getAttackDamage() << " points of damage!\n";
+		std::cout << "ScavTrap " << getName() << " punched " << target << ", dealing " << getAttackDamage() << " damage!\n";
 		setEnergyPoints(getEnergyPoints() - 1);
 	}
 };
