cf6ad53e53a1c4eddb060f12de069572d3e5513e
Ny Hasina <rarakoto@c1r10s14.42antananarivo.mg>
2025-01-06 13:27:05 +0300

    ex02 Done

--- cpp_module_4/ex02/Animal.hpp
+++ cpp_module_4/ex02/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 08:46:18 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/06 11:08:51 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/06 13:25:05 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 class Animal
 {
 protected:
-	std::string	type;
-
-public:
 	Animal(void);
 	Animal(std::string type);
 	Animal(const Animal &other);
-	Animal	&operator=(const Animal &other);
+	std::string	type;
+
+public:
 	virtual ~Animal(void);
+	Animal	&operator=(const Animal &other);
 	std::string		getType(void) const;
 	void			setType(std::string new_type);
 	virtual void	makeSound(void) const;
