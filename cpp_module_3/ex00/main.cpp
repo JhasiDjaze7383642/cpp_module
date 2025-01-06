e3a64051ac7a57955e0b650e65c57b20da4261e5
Ny Hasina <rarakoto@c1r10s16.42antananarivo.mg>
2025-01-06 10:15:58 +0300

    tested something on ex00

--- cpp_module_3/ex00/main.cpp
+++ cpp_module_3/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 08:48:11 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/06 10:14:19 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:15:34 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(void)
 	a.attack("Violence");
 	a.attack("Another");
 	a.attack("Again");
-	a.takeDamage(20);
+	a.takeDamage(1);
 	a.attack("UI");
 	a.beRepaired(2);
 	std::cout << a.getEnergyPoints() << std::endl;
