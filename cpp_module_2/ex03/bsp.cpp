3361cf25766b20f397fd5ce0b594729f0b2dcdf2
Ny Hasina <rarakoto@c1r8s7.42antananarivo.mg>
2025-01-08 13:27:09 +0300

    Changed the prototype of bsp

--- cpp_module_2/ex03/bsp.cpp
+++ cpp_module_2/ex03/bsp.cpp
@@ -6,22 +6,22 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 07:49:15 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/08 13:22:47 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/08 13:26:30 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-float crossProduct(const Point& A, const Point& B, const Point& C)
+float crossProduct(const Point& a, const Point& b, const Point& c)
 {
-    return ((B.getX() - A.getX()) * (C.getY() - A.getY()) - (B.getY() - A.getY()) * (C.getX() - A.getX())).toFloat();
+    return ((b.getX() - a.getX()) * (c.getY() - a.getY()) - (b.getY() - a.getY()) * (c.getX() - a.getX())).toFloat();
 }
 
-bool bsp(const Point A, const Point B, const Point C, const Point P)
+bool bsp(const Point a, const Point b, const Point c, const Point point)
 {
-    float cross1 = crossProduct(A, B, P);
-    float cross2 = crossProduct(B, C, P);
-    float cross3 = crossProduct(C, A, P);
+    float cross1 = crossProduct(a, b, point);
+    float cross2 = crossProduct(b, c, point);
+    float cross3 = crossProduct(c, a, point);
     bool hasSameSign1 = (cross1 >= 0 && cross2 >= 0 && cross3 >= 0);
     bool hasSameSign2 = (cross1 <= 0 && cross2 <= 0 && cross3 <= 0);
 
