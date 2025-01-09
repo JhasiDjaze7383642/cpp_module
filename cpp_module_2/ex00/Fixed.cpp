6567118c820ed66d0f2e79e7a32353f77bcaa7c1
Ny Hasina <rarakoto@c1r10s12.42antananarivo.mg>
2025-01-09 09:26:10 +0300

    Cleaned this exo

--- cpp_module_2/ex00/Fixed.cpp
+++ cpp_module_2/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 09:20:15 by rarakoto          #+#    #+#             */
-/*   Updated: 2024/12/18 07:05:46 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/09 09:23:22 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 
 Fixed::Fixed(void) : value(0), fractionnal_value(8)
 {
-	std::cout << "Default constructor called.\n";
+	std::cout << "Default constructor called\n";
 };
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Deconstructor called.\n";
+	std::cout << "Destructor called\n";
 }
 
 Fixed::Fixed(const Fixed &other) : value(other.value), fractionnal_value(8)
 {
-	std::cout << "Copy constructor called.\n";
-	operator=(other);
+	std::cout << "Copy constructor called\n";
 };
 
 Fixed&	Fixed::operator=(const Fixed &other)
 {
-	std::cout << "Copy assignement operator called.\n";
+	std::cout << "Copy assignment operator called\n";
 	if (this != &other)
 		this->value = other.getRawBits();
 	return (*this);
@@ -38,7 +37,7 @@ Fixed&	Fixed::operator=(const Fixed &other)
 
 int		Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called.\n";
+	std::cout << "getRawBits member function called\n";
 	return (this->value);
 }
 
