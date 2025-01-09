3f8070d8e7d7ccfc63feba77ac22b8c51e3e7784
Ny Hasina <rarakoto@c1r10s12.42antananarivo.mg>
2025-01-09 10:02:48 +0300

    changed the main to something that is inside

--- cpp_module_2/ex03/main.cpp
+++ cpp_module_2/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 10:46:16 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/08 13:19:40 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/09 10:00:03 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(void)
 	Point	A (0.0f, 0.0f);
 	Point	B (5.0f, 0.0f);
 	Point	C (0.0f, 5.0f);
-	Point	P (2.0f, 3.4f);
+	Point	P (2, 2.85f);
 
 	if (bsp(A, B, C, P))
 		std::cout << "Inside\n";
