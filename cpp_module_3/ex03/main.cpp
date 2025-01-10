64432348ec01b81810dff70dc679094f1e576991
Ny Hasina <rarakoto@c1r9s9.42antananarivo.mg>
2025-01-10 09:57:53 +0300

    Added more test

--- cpp_module_3/ex03/main.cpp
+++ cpp_module_3/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 08:48:11 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/10 09:47:46 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/10 09:56:47 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,8 @@ int main(void)
 	a.attack("Harena");
 	a.attack("Harena");
 	a.attack("Harena");
-	a.takeDamage(200);
+	a.takeDamage(78);
+	a.takeDamage(500);
 	a.attack("Harena");
 	a.attack("Harena");
 	a.attack("Harena");
@@ -170,5 +171,8 @@ int main(void)
 	a.guardGate();
 	a.highFivesGuys();
 	a.whoAmI();
+	std::cout << "My Hit points are " << a.getHitPoints() << std::endl;
+	std::cout << "My Energy points are " << a.getEnergyPoints() << std::endl;
+	std::cout << "My Attack Damage are " << a.getAttackDamage() << std::endl;
 	return (0);
 }
