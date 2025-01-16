1796cbaa81c710d90bf3f9f7da884612b70bbb92
Ny Hasina <djazejhasi@gmail.com>
2025-01-16 14:44:08 +0300

    Updated the test to match the one in the previous exercice

--- cpp_module_4/ex02/main.cpp
+++ cpp_module_4/ex02/main.cpp
@@ -6,30 +6,35 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 08:59:46 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/06 13:07:04 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/16 14:41:14 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
+# define SPAWN_NB 100
 
 int main()
 {
-	// const Animal	*array[100];
-	// unsigned int	pos;
-	//
-	// pos = -1;
-	// while (++pos < 50)
-	// 	array[pos] = new Dog();
-	// while (++pos < 100)
-	// 	array[pos] = new Cat();
-	// pos = 0;
-	// while (pos < 100)
-	// 	delete array[pos++];
-	const Animal	*j = new Dog();
-	const Animal	*i = new Cat();
-
-	delete j;
-	delete i;
+	{
+		const Animal	*array[SPAWN_NB];
+		short			pos;
+		
+		pos = -1;
+		while (++pos < SPAWN_NB / 2)
+			array[pos] = new Dog();
+		while (pos < SPAWN_NB)
+			array[pos++] = new Cat();
+		pos = -1;
+		while (++pos < SPAWN_NB)
+			delete array[pos];
+	}
+	{
+		const Animal	*j = new Dog();
+		const Animal	*i = new Cat();
+		
+		delete j;
+		delete i;
+	}
 	return (0);
 }
