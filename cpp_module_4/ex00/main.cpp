a51965d9c70a30a3432dd9e644f661aaf20be47f
Ny Hasina <djazejhasi@gmail.com>
2025-01-16 13:38:49 +0300

    Removed wrongmain.cpp and merged it directly inside main.cpp

--- cpp_module_4/ex00/main.cpp
+++ cpp_module_4/ex00/main.cpp
@@ -6,26 +6,41 @@
 /*   By: rarakoto <rarakoto@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 08:59:46 by rarakoto          #+#    #+#             */
-/*   Updated: 2025/01/06 10:46:26 by rarakoto         ###   ########.fr       */
+/*   Updated: 2025/01/16 13:36:17 by rarakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete meta;
-	delete j;
-	delete i;
+	{
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+		delete meta;
+		delete j;
+		delete i;
+	}
+	{
+		// const WrongAnimal* meta = new WrongAnimal();
+		// const WrongAnimal* i = new WrongCat();
+		// WrongCat	cat;
+		// std::cout << i->getType() << " " << std::endl;
+		// i->makeSound();
+		// meta->makeSound();
+		// cat.makeSound();
+		// delete meta;
+		// delete i;
+	}
 	return (0);
 }
